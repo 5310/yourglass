c7cbf77d4bdbad2e5b714b3f85742fb5eefbaa2a
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2011-01-26 17:10:36 +0530

    Added hourglass base with basic controls.

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 Yourglass: A Game of Giving
 ===========================
-+yourglass -game
 
 
-Introduction
+Controls
 ------------
 
-TBW
+Direction keys.
+-	Left/right to rotate.
+-	Down to be more insistent!
 
 
 Running the Game
@@ -17,9 +18,25 @@ Therefore, in order to run, do the following:
 
 1.	First, install [LÖVE][1] for you system, if you haven't.
 2.	From the `yourglass` directory run your system's relevant runner script; eg `run.sh` on Linux.
-3.	Et voila, it runs! Impressive eh?
+3.	Et voilà, it runs! Impressive eh?
 
+
+Gitting Stuff
+-------------
+
+Keep forgetting git? This section may help. Or you may try the [cheat-sheet][2].
+
+-	`git add .` will add all files from the working directory to the index.
+-	**`git add -u` will update your tracked files to the index, effectively taking a snapshot.**
+-	`git diff` and `git cheackout` will diff from revert you to the index.
+-	**`git commit -a -m "Commit message."` will update all your tracked changes to the index, and then commit it to the local repo.**
+-	`git commit -m "Commit message."` will commit the current index to the local repo.
+-	`git diff HEAD` and `git checkout HEAD` will diff from and revert you to the local-repo.
+-	`git stash save "Stash message."` will stash your current working directory.
+-	**`git push`** will push your local repo's last commit to the remote repo, if it's fresh!
+-	**`git pull origin master` will get merge the remote repo's current state to your working directory.**
 
 - - -
 
-[1]: http://love2d.org/		"LÖVE"
+[1]: http://love2d.org/								"LÖVE"
+[2]: http://help.github.com/git-cheat-sheets/		"Github Git Cheat-sheet"
--- main.lua
+++ main.lua
@@ -1,15 +1,114 @@
+--utility
+function hsl(h, s, l)   --http://love2d.org/wiki/HSL_color
+    if s <= 0 then return l,l,l end
+    h, s, l = h/256*6, s/255, l/255
+    local c = (1-math.abs(2*l-1))*s
+    local x = (1-math.abs(h%2-1))*c
+    local m,r,g,b = (l-.5*c), 0,0,0
+    if h < 1     then r,g,b = c,x,0
+    elseif h < 2 then r,g,b = x,c,0
+    elseif h < 3 then r,g,b = 0,c,x
+    elseif h < 4 then r,g,b = 0,x,c
+    elseif h < 5 then r,g,b = x,0,c
+    else              r,g,b = c,0,x
+    end return math.ceil((r+m)*256),
+               math.ceil((g+m)*256),
+               math.ceil((b+m)*256)
+end
+
+
 function love.load()
 
-    width = 800
-    height = 500
+
+    --setup
+    width =     1600
+    height =    900
+    scale = 0.5
+    width = width*scale
+    height = height*scale
     fullscreen = false
     love.graphics.setMode(width, height, fullscreen)
+    love.graphics.setBackgroundColor( hsl(138, 20, 50) )
+    
+    
+    --physics
+    gravity =   0
+    meter =     50
+    world = love.physics.newWorld(0, 0, width*3, height*3)
+    world:setGravity(0, gravity)
+    world:setMeter(meter)
+    origin = love.physics.newBody(world, width/2, height/2, 0, 0)
+    
+    
+    --glass
+    glass = {}
+    
+    glass.body = love.physics.newBody(world, width/2, height/2, 100, 1000)
+    glass.joint = love.physics.newRevoluteJoint(origin, glass.body, origin:getX(), origin:getY())
+    glass.body:setAngularDamping(5)
+
+    function glass:draw()
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.translate(self.body:getX(), self.body:getY())
+        love.graphics.rotate(self.body:getAngle())
+        
+        --HAVE to simplify
+        love.graphics.polygon('fill',  18*scale, 0*scale,  164*scale, 230*scale,  210*scale, 230*scale,  64*scale, 0*scale,  210*scale, -230*scale,  164*scale, -230*scale)
+        love.graphics.polygon('fill', -18*scale, 0*scale, -164*scale, 230*scale, -210*scale, 230*scale, -64*scale, 0*scale, -210*scale, -230*scale, -164*scale, -230*scale)
+        love.graphics.polygon('fill', -210*scale,  230*scale, 210*scale,  230*scale, 236*scale,  270*scale, -236*scale,  270*scale)
+        love.graphics.polygon('fill', -210*scale, -230*scale, 210*scale, -230*scale, 236*scale, -270*scale, -236*scale, -270*scale)
+        
+    end
+    
+    
+    --gentleman "Class", not yet ready for instancing
+    gentleman = {}
+    gentleman.insist = 1
+    gentleman.force = 50
+    gentleman.impulse = 100*scale
+    gentleman.state = 0
+    gentleman.delay = 0
+    gentleman.impatience = 0
+    gentleman.control = {right="right", down="down", left="left"}
+    function gentleman:update(dt)
+        right = love.keyboard.isDown(self.control.right)
+        left = love.keyboard.isDown(self.control.left)
+        down = love.keyboard.isDown(self.control.down)
+        if right and self.state <= 0 and self.impatience == 0 then
+            glass.body:applyImpulse(-gentleman.impulse * self.insist, 0, 0, gentleman.impulse)
+            self.state = self.insist/self.force*self.delay
+            self.insist = 1
+            self.impatience = 1
+        elseif left and self.state <= 0 and self.impatience == 0 then
+            glass.body:applyImpulse(gentleman.impulse * self.insist, 0, 0, gentleman.impulse)
+            self.state = self.insist/self.force*self.delay
+            self.insist = 1
+            self.impatience = 1
+        elseif down then
+            self.insist = self.insist + self.force*dt
+        end
+        
+        if not right and not left and not down then
+            self.state = self.state - dt
+            self.impatience = 0
+        end
+    end
+    
     
 end
 
 function love.update(dt)
+    world:update(dt)
+    --here we are going to create some keyboard events
+    gentleman:update(dt)
+
+    
 end
 
 function love.draw()    
-    love.graphics.print('Gentlemen, start your givings!', 10, 10)
+    --Good evening gentlemen of the world.
+    love.graphics.print(gentleman.insist, 10, 10)
+    
+    --glass
+    glass:draw()
 end
